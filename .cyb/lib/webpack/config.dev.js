f2c6a0d004141c60bc2349325c0b2b1e6a6d4938
furic <furic@qq.com>
2018-08-12 16:00:21 +0800

    打包逻辑调整

--- .cyb/lib/webpack/config.dev.js
+++ .cyb/lib/webpack/config.dev.js
@@ -7,8 +7,6 @@
 
 const webpack = require('webpack')
 const path = require('path')
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
-const notifier = require('node-notifier')
 const outputPath = require('./output-path')
 const config = require('../fezconfig')
 
@@ -16,27 +14,12 @@ module.exports = {
   mode: 'development',
   output: {
     path: outputPath.dev(),
+    filename: '[name].js',
+    chunkFilename: path.join(outputPath.js(), '[name].js'),
     publicPath: '/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new FriendlyErrorsWebpackPlugin({
-      compilationSuccessInfo: {
-        messages: [],
-      },
-      onErrors: (severity, errors) => {
-        if (severity !== 'error') {
-          return;
-        }
-        const error = errors[0];
-        notifier.notify({
-          title: `${config.projectName} 编译出错`,
-          message: severity + ': ' + error.name,
-          subtitle: error.file || '',
-          icon: path.join(__dirname, '../', 'cyb-logo.png')
-        });
-      }
-    })
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     rules: [{
