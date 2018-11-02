901a4cc95966a44880b1db2e6fcfa64fe2d1fdab
furic <furic@qq.com>
2018-11-02 11:42:07 +0800

    修复压缩html配置无效问题

--- .cyb/dist.js
+++ .cyb/dist.js
@@ -693,18 +693,7 @@ module.exports = () => {
         return usemin({
           css: [minifyCSS()],
           html: [() => {
-            const options = {}
-            Object.assign(options, config.useHtmlMin.options, {
-              removeComments: true, //清除HTML注释
-              collapseWhitespace: true, //压缩HTML
-              collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input checked />
-              removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
-              removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
-              removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
-              minifyJS: true, //压缩页面JS
-              minifyCSS: true //压缩页面CSS
-            })
-            return htmlmin(options)
+            return htmlmin(config.useHtmlMin.options)
           }],
           js: [uglify()],
           assetsDir: './src/'
